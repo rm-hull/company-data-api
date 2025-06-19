3f3591594894ad91e82789dcf52be2e05d956dd2
Richard Hull <rm_hull@yahoo.co.uk>
2025-06-19 23:33:28 +0100

    Search, group results by postcode

--- internal/search.go
+++ internal/search.go
@@ -17,6 +17,11 @@ type SearchResponse struct {
 	Attribution []string                         `json:"attribution"`
 }
 
+type GroupedSearchResponse struct {
+	Results     map[string][]models.CompanyDataWithLocation `json:"results"`
+	Attribution []string                                    `json:"attribution"`
+}
+
 func Search(repo repo.SearchRepository) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		bbox, err := parseBBox(c.Query("bbox"))
@@ -37,6 +42,36 @@ func Search(repo repo.SearchRepository) func(c *gin.Context) {
 		}
 
 		c.JSON(http.StatusOK, SearchResponse{
+			Results:     results,
+			Attribution: ATTRIBUTION,
+		})
+	}
+}
+
+func GroupByPostcode(repo repo.SearchRepository) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		bbox, err := parseBBox(c.Query("bbox"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		results := make(map[string][]models.CompanyDataWithLocation, 100)
+		err = repo.Find(bbox, func(companyData *models.CompanyDataWithLocation) {
+			arr, exists := results[companyData.RegAddressPostCode]
+			if !exists {
+				arr = make([]models.CompanyDataWithLocation, 0, 10)
+			}
+			results[companyData.RegAddressPostCode] = append(arr, *companyData)
+		})
+
+		if err != nil {
+			log.Printf("error while fetching company data: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "An internal server error occurred"})
+			return
+		}
+
+		c.JSON(http.StatusOK, GroupedSearchResponse{
 			Results:     results,
 			Attribution: ATTRIBUTION,
 		})
